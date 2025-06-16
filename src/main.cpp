65eb88e909eeaacedd268195de22371f1db87d47
Salanto <62221668+Salanto@users.noreply.github.com>
2025-06-16 03:21:21 +0200

    Fixup dynamic dock creation

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,44 @@
+#include "../modules/audio/src/audio.h"
+#include "../modules/audio/src/audiobackend.h"
 #include <QApplication>
 #include <QDebug>
 #include <QQmlApplicationEngine>
+#include <kddockwidgets/Config.h>
 #include <kddockwidgets/qtquick/Platform.h>
+
+#include <QDebug>
+#include <QTimer>
 
 int main(int argc, char *argv[])
 {
   QApplication app(argc, argv);
+
   QQmlApplicationEngine engine;
   KDDockWidgets::initFrontend(KDDockWidgets::FrontendType::QtQuick);
+
+  KDDockWidgets::Config::Flags bar;
+  KDDockWidgets::Config::self().setFlags(bar);
+  KDDockWidgets::Config::self().setSeparatorThickness(5);
+
+  qDebug() << bar;
+
   KDDockWidgets::QtQuick::Platform::instance()->setQmlEngine(&engine);
 
-  const QUrl url = QUrl::fromLocalFile(QStringLiteral("./files/main.qml"));
+  const QUrl url = QUrl::fromLocalFile(QStringLiteral("./files/system/main.qml"));
   QObject::connect(&engine, &QQmlApplicationEngine::objectCreated, &app, [url](QObject *obj, const QUrl &objUrl) {
     if (!obj && url == objUrl)
       QCoreApplication::exit(-1);
   }, Qt::QueuedConnection);
 
-  // auto control = engine.singletonInstance<Audio *>("Attorney.Audio", "Audio");
-  // control->setBackend(new AudioBackend(&engine));
+  auto control = engine.singletonInstance<Audio *>("Attorney.Audio", "Audio");
+  control->setBackend(new AudioBackend(&engine));
 
   engine.load(url);
+
+  QTimer *timer = new QTimer(&app);
+  timer->setSingleShot(false);
+  timer->setInterval(100);
+  timer->callOnTimeout([&engine]() { engine.clearComponentCache(); });
+  timer->start();
   return app.exec();
 }
